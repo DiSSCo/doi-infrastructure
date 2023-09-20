83ea5c2bb4b73cfe608ea2c165ab347d9343fcc5
southeo <soulaine.theo@gmail.com>
2023-09-20 08:38:06 +0200

    security group

--- infrastructure/ec2/main.tf
+++ infrastructure/ec2/main.tf
@@ -1,18 +1,20 @@
 provider "aws" {
   region = "eu-west-2"
-  tags   = {
-    Environment = "DOI"
-    Owner       = "DiSSCo"
-    Project     = "DiSSCo PID"
-    Terraform   = "True"
+  default_tags {
+    tags = {
+      Environment = "DOI"
+      Owner       = "DiSSCo"
+      Project     = "DiSSCo PID"
+      Terraform   = "True"
+    }
   }
 }
 
-module "handle_server" {
-  source        = "terraform-aws-modules/ec2-instance/aws"
-  ami           = "ami-0a244485e2e4ffd03"
-  instance_type = "t3.small"
-  azs = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-
-
+resource "aws_instance" "doi_server" {
+  source          = "terraform-aws-modules/ec2-instance/aws"
+  ami             = "ami-0a244485e2e4ffd03"
+  instance_type   = "t3.small"
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  security_groups = [aws_security_group.doi-server-sg.id]
+  subnet_id       = doi_server_subnet_group
 }
